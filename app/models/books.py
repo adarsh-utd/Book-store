f9548fad1aa3ce111afa33f31599766152fde7a2
adarsh-utd <adarsh99.mec@gmail.com>
2023-04-18 13:53:30 +0530

    Add view all books endpoint

--- app/models/books.py
+++ app/models/books.py
@@ -3,6 +3,8 @@ from typing import Optional
 from bson import ObjectId
 from pydantic import BaseModel, Field
 
+from app.models.base import PyObjectId
+
 
 class BookRequestModel(BaseModel):
     name: str
@@ -13,3 +15,24 @@ class BookRequestModel(BaseModel):
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
+
+
+class Books(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str
+    description: str
+    image: Optional[str] = None
+    is_deleted: bool = False
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+    def list_books(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "description": self.description,
+            "image": self.image
+        }
--- app/router/book.py
+++ app/router/book.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from starlette import status
 
 from app.db.base import books_collection
-from app.models.books import BookRequestModel
+from app.models.books import BookRequestModel, Books
 from app.utils.utils import get_error_response, get_current_active_user
 
 router = APIRouter(
@@ -32,5 +32,23 @@ async def create_book(request: BookRequestModel, user: object = Depends(get_curr
     response = {
         "status": True,
         "message": 'Book added'
+    }
+    return response
+
+
+@router.get('/list-books')
+async def list_books(user: object = Depends(get_current_active_user)):
+    if user is None:
+        return get_error_response("Unauthorized",
+                                  status.HTTP_401_UNAUTHORIZED)
+
+    find_query = {
+        "is_deleted": False
+    }
+    book_list = list(books_collection.find(find_query))
+    books = [Books(**x).list_books() for x in book_list]
+
+    response = {
+        "books": books
     }
     return response
