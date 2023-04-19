9278992ee291033fa20af392c0c929288f7e78ab
adarsh-utd <adarsh99.mec@gmail.com>
2023-04-19 12:43:16 +0530

    Add customer with favourite book endpoint

--- app/router/customer.py
+++ app/router/customer.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from starlette import status
 
-from app.db.base import customers_collection
+from app.db.base import customers_collection, favourite_books_collection
 from app.models.customers import CustomerModel
 from app.utils.utils import get_current_active_user, get_error_response
 
@@ -26,5 +26,26 @@ async def customer_list(user: object = Depends(get_current_active_user)):
     customers = [CustomerModel(**x).customer_list_response() for x in customers_list]
     response = {
         "customers": customers
+    }
+    return response
+
+
+@router.get('/customers-with-favorite-books')
+async def get_customers_with_favourite_books_count(user: object = Depends(get_current_active_user)):
+    find_query = {
+        "is_deleted": False
+    }
+    customers_list = list(customers_collection.find(find_query))
+
+    favourite_books = list(favourite_books_collection.find())
+    customers_response_list = []
+    for customer in customers_list:
+        favourite_book = next((a.get("favourite_books_list") for a in favourite_books
+                               if a.get('customer_id') == customer.get('_id')), None)
+        customer_response = CustomerModel(**customer).customer_list_response()
+        customer_response['favourite_book_count'] = len(favourite_book.split(','))
+        customers_response_list.append(customer_response)
+    response = {
+        "customer_response": customers_response_list
     }
     return response
